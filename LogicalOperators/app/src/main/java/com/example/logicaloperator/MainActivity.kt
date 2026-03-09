5f7e68a97643b08acb7497acafb2e0a6cf5b658c
ntandodlamini <ST10525952@myemeris.edu.za>
2026-03-09 09:46:59 +0200

    changes
    declares

--- LogicalOperators/app/src/main/java/com/example/logicaloperator/MainActivity.kt
+++ LogicalOperators/app/src/main/java/com/example/logicaloperator/MainActivity.kt
@@ -41,6 +41,62 @@ private lateinit var tvResult: EditText
         tvResult = findViewById(R.id.tvResult)
 
 
+        //button to run the code when it is pressed
+        btnCheck.setOnClickListener {
+
+            //get the user's name from the editText
+            val name = editName.text.toString()
+
+            //convert the age entered into a number (int)
+            val age = editAge.text.toString().toInt()
+
+            //check if checkboxes are selected(true or false)
+            val isITStudent = cbITStudent.isChecked
+            val isStaff = cbStaff.isChecked
+            val isBanned = cbBanned.isChecked
+
+            if (((age>= 18 && isITStudent)|| isStaff)&& !isBanned){
+
+                //if the condition above is true
+                tvResult.text = "Congratulations $name! You qualify for the student tech discount."
+
+            }else {
+
+                //if the condition above is false
+                tvResult.text = "Sorry $name, you do not qualify for the discount "
+
+             /*
+             Logical condition explanation
+
+                    Step 1:
+                    (age >= 18 && isITStudent)
+                    >= means greater than equal to
+                    && means AND (both conditions must be true)
+
+                    so this checks if the user is 18 or older and is an IT student
+
+
+                    Step 2;
+                    || means OR (only one condition needs to be true)
+
+                    This means the person  can qualify if they are a staff member even if they are
+                    not an IT student
+
+                    Step 3: && !isBanned
+                    ! means NOT (it reverses the value)
+                    !isBanned means the person must NOT be banned
+
+                    Example
+                    isBanned = false
+                    !false = true
+
+              */
+            }
+
+
+        }
+
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
