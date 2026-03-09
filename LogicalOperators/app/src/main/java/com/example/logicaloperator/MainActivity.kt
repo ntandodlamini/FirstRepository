ee96e5ba2b83e87dc92a9f148a5d3ccc70941f16
ntandodlamini <ST10525952@myemeris.edu.za>
2026-03-09 08:35:20 +0200

    changes
    declaring variables

--- LogicalOperators/app/src/main/java/com/example/logicaloperator/MainActivity.kt
+++ LogicalOperators/app/src/main/java/com/example/logicaloperator/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.example.logicaloperator
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
+
+private lateinit var editName: EditText
+private lateinit var editAge: EditText
+private lateinit var cbStaff: CheckBox
+private lateinit var cbITStudent: CheckBox
+private lateinit var cbBanned: CheckBox
+private lateinit var btnCheck: Button
+private lateinit var tvResult: EditText
+
+
+
+
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
